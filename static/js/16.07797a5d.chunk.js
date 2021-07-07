18d9546907d1acb8ef4141ebee815939bdca40d7
leo chan <leochan007@163.com>
2021-07-08 02:34:34 +0800

    update webapp

--- static/js/16.07797a5d.chunk.js
+++
@@ -1,2 +0,0 @@
-(this["webpackJsonp@loopring-web/webapp"]=this["webpackJsonp@loopring-web/webapp"]||[]).push([[16],{2222:function(p,o){}}]);
-//# sourceMappingURL=16.07797a5d.chunk.js.map
---
+++ static/js/16.5989df4f.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonp@loopring-web/webapp"]=this["webpackJsonp@loopring-web/webapp"]||[]).push([[16],{2231:function(p,o){}}]);
+//# sourceMappingURL=16.5989df4f.chunk.js.map
